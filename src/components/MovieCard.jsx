d5f6894b23fed67b24088d2d732a1c9d569cb66b
Rajaabpro <rajaab.pro@gmail.com>
2025-05-12 19:03:12 +0500

    feat(MovieCard): add new MovieCard component for displaying movie details

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MovieCard = () => {
+  return (
+    <div>
+        <p key={MovieCard.id} className='text-white'>{MovieCard.title}</p>
+      
+    </div>
+  )
+}
+
+export default MovieCard
