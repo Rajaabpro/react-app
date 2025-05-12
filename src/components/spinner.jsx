6cabeee090606804688f98ce3e22bf54c8dcaeab
Rajaabpro <rajaab.pro@gmail.com>
2025-05-12 18:45:15 +0500

    feat(Spinner): add a new spinner component for loading indication

---
+++ src/components/spinner.jsx
@@ -0,0 +1,11 @@
+// src/components/Spinner.jsx
+const Spinner = () => {
+    return (
+      <div className="flex justify-center items-center py-10">
+        <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-blue-500 border-t-transparent"></div>
+      </div>
+    );
+  };
+  
+  export default Spinner;
+  
