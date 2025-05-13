ad512d62a03c286e53df9c4fba4329180563948b
Rajaabpro <rajaab.pro@gmail.com>
2025-05-13 14:15:12 +0500

    feat(MovieCard): update MovieCard component to accept movie props and enhance layout with image and title

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
-const MovieCard = () => {
+const MovieCard = ({ movie:
+  { title, vote_average, poster_path, release_date, original_language }
+}) => {
   return (
-    <div>
-        <p key={MovieCard.id} className='text-white'>{MovieCard.title}</p>
-      
-    </div>
+    <div className="movie-card">
+      <img
+        src={poster_path ?
+          `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
+        alt={title}
+      />
+      <div className='mt-4'>
+        <h3>{title}</h3>
+        <div className='content'>
+          <div className='rating'></div>
+        </div>
+      </div>
+      </div>
   )
 }
 
